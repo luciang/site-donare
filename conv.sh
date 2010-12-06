be30831125b897dec45d15ee49e0ff9810812387
lucian <lucian@lethe.(none)>
2010-12-06 03:54:11 +0200

    init

---
+++ conv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for f in orig/*.png; do
+    name=`basename $f`
+    name_no_ext=`basename $f .png`
+    convert -flatten orig/$name flat/$name
+    pngtopnm < flat/$name > pnm/$name_no_ext.pnm
+    potrace < pnm/$name_no_ext.pnm > eps/$name_no_ext.eps
+    convert -size 320x320  eps/$name_no_ext.eps dst/$name_no_ext.png
+done
